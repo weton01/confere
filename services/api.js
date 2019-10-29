ae3c42c5b14ab1f52b447333b14883d9c8abbf0f
Weton01 <wellington_ferreira280@hotmail.com>
2019-10-29 10:33:51 -0300

    make it better

--- services/api.js
+++ services/api.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const variables = require("../bin/configuration/variables");
 
-var url = `http://localhost:${variables.Api.port}`;
+var url = `https://confere-project.herokuapp.com`;
 
 const api = axios.create({
 	baseURL: url
